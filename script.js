3d8440a4eeee4b2ab2000a7683fafd1ebcde2f83
Pruthviraj Chougale <pruthvirajchougale17@gmail.com>
2024-06-12 22:58:35 +0530

    improved

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let div=document.querySelector('div');
 let cityinput=document.querySelector('#searchweather');
 let click=document.querySelector('#search');
 let refresh=document.querySelector('#refresh');
+let icon=document.querySelector('#icon');
 const APIkey='ad4eb5a45f8c42c857e3f7232cd98292';
 
 let temperature=document.querySelector('#temp');
@@ -19,12 +20,36 @@ const checkweather = async (input) => {
     feelslike.innerHTML+=`<h4>${Math.round(response.main.feels_like-273.15)} deg</h4>`;
     humidity.innerHTML+=`<h4>${response.main.humidity}</h4>`;
     weather.innerHTML+=`<h4>${response.weather[0].main}</h4>`;
+    if(response.weather[0].main=="Clouds"){
+        icon.innerHTML=`<img src="images/clouds.png"></img>`;
+    }
+    if(response.weather[0].main=="Drizzle"){
+        icon.innerHTML=`<img src="images/drizzle.png"></img>`;
+    }
+    if(response.weather[0].main=="Clear"){
+        icon.innerHTML=`<img src="images/clear.png"></img>`;
+    }
+    if(response.weather[0].main=="Mist"){
+        icon.innerHTML=`<img src="images/mist.png"></img>`;
+    }
+    if(response.weather[0].main=="Rain"){
+        icon.innerHTML=`<img src="images/rain.png"></img>`;
+    }
+    if(response.weather[0].main=="Snow"){
+        icon.innerHTML=`<img src="images/snow.png"></img>`;
+    }
+
 }
 
 click.addEventListener('click',(e)=>{
     e.preventDefault();
     let searchinput=cityinput.value;
+    temperature.innerHTML=` <P>Temperature : </P>`;
+    feelslike.innerHTML=` <p>Feels like : </p>`;
+    humidity.innerHTML=`<p>Humidity : </p>`;
+    weather.innerHTML=`<p>Weather : </p>`;
     checkweather(searchinput);
+    console.log(response);
 });
 
 refresh.addEventListener('click',(e)=>{
